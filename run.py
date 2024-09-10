a311cde2e508ca27dbf09e54f247bcf9c4c456a6
Chrysanthus Chiagwah <chrysanthusobinna@gmail.com>
2024-09-10 04:48:37 +0000

    update  gspread and google-auth libraries to enable Google Sheets API access

--- run.py
+++ run.py
@@ -13,8 +13,10 @@ creds = Credentials.from_service_account_file("creds.json", scopes=SCOPE)
 client = gspread.authorize(creds)
 
 # Open the spreadsheet by name
-SHEET = client.open("hotel-app")  # open spreadsheet 
-WORKSHEET = SHEET.booking-record  # Access  booking-record sheet
+CREDS = Credentials.from_service_account_file('creds.json')
+SCOPED_CREDS = CREDS.with_scopes(SCOPE)
+GSPREAD_CLIENT = gspread.authorize(SCOPED_CREDS)
+SHEET = GSPREAD_CLIENT.open('hotel-app')
 
 
 def add_guest(name, phone, address, email, room_class, room_number, amount_paid):
@@ -24,3 +26,5 @@ def add_guest(name, phone, address, email, room_class, room_number, amount_paid)
     guest_data = [name, phone, address, email, room_class, room_number, amount_paid]
     WORKSHEET.append_row(guest_data)
     print(f"Added guest: {name}")
+
+add_guest(name, phone, address, email, room_class, room_number, amount_paid)
