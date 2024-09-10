d0f757f2b8cddee374fd41ef06df9f991595e5ff
Chrysanthus Chiagwah <chrysanthusobinna@gmail.com>
2024-09-10 04:34:38 +0000

    Set up Google Sheets API using gspread and service account credentials

--- run.py
+++ run.py
@@ -1,3 +1,17 @@
-# Your code goes here.
-# You can delete these comments, but do not change the name of this file
-# Write your code to expect a terminal of 80 characters wide and 24 rows high
+import gspread
+from google.oauth2.service_account import Credentials
+
+# Google Sheets API setup
+SCOPE = [
+    "https://www.googleapis.com/auth/spreadsheets",
+    "https://www.googleapis.com/auth/drive.file",
+    "https://www.googleapis.com/auth/drive"
+    ]
+
+# Load credentials
+creds = Credentials.from_service_account_file("creds.json", scopes=SCOPE)
+client = gspread.authorize(creds)
+
+# Open the spreadsheet by name
+SHEET = client.open("Hotel Management")  # open spreadsheet 
+WORKSHEET = SHEET.booking-record  # Access  booking-record sheet
