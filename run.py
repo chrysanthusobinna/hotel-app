a19ca77641fd6bc68b482db5dc8ecf93e6fadd65
Chrysanthus Chiagwah <chrysanthusobinna@gmail.com>
2024-09-10 05:22:03 +0000

    fix function add_guest and test function

--- run.py
+++ run.py
@@ -17,6 +17,9 @@ CREDS = Credentials.from_service_account_file('creds.json')
 SCOPED_CREDS = CREDS.with_scopes(SCOPE)
 GSPREAD_CLIENT = gspread.authorize(SCOPED_CREDS)
 SHEET = GSPREAD_CLIENT.open('hotel-app')
+WORKSHEET = "booking-record"
+
+
 
 
 def add_guest(name, phone, address, email, room_class, room_number, amount_paid):
@@ -24,7 +27,8 @@ def add_guest(name, phone, address, email, room_class, room_number, amount_paid)
     Adds a new guest entry to the spreadsheet.
     """
     guest_data = [name, phone, address, email, room_class, room_number, amount_paid]
-    WORKSHEET.append_row(guest_data)
+    SHEET.worksheet(WORKSHEET).append_row(guest_data) 
     print(f"Added guest: {name}")
 
-add_guest(name, phone, address, email, room_class, room_number, amount_paid)
+
+add_guest("chrys", "08026044909", "bolton, UK", "chrys@gmail.com", "Gold Room", "5", "500")
